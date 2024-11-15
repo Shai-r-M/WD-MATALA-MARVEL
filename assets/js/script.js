413839a82cc061469627cf39b5afb32faff6aa71
Shai-r-M <shairamatala@gmail.com>
2024-11-15 12:36:10 +0800

    ha5

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var tl = gsap.timeline(), 
+
+split = new SplitText("#quote", {type:"words,chars"}), 
+
+words = split.words; //an array of all the divs that wrap each character
+
+tl.to(words, {
+  duration: 7,
+  "--weight": "300",
+  ease: "power3.out",
+  color: "hsl(+=0, +=70%, +=20%)",
+  stagger: {
+    each: 0.1,
+  }
+})
+
