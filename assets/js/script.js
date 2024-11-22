81859f7fc35218d2821169b39681714229ea9bd7
Shai-r-M <shairamatala@gmail.com>
2024-11-22 13:34:05 +0800

    ha9

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,37 @@
-var tl = gsap.timeline(),
-  split = new SplitText("#quote", { type: "words,chars" }),
-  words = split.words;
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
-tl.to(words, {
-  duration: 7,
-  "--weight": "300",
-  ease: "power3.out",
-  color: "hsl(+=0, +=70%, +=20%)",
-  stagger: {
-    each: 0.1
-  }
-});
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  
 
 
 
